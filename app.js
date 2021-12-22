0f2ab64a86aa5308e72050807cc66aedebd335f5
rahulc007 <rahul.chandranna@gmail.com>
2021-12-22 15:57:42 +0530

    Modified test api response

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const route = require('./routes')
 
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use('/ping', (req,res,next) => {
-  res.send("Server is working!!!.")
+app.use('/ping', (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "Api running successfully."
+  })
 })
 
 route(app);
